66526a73014dffe22ea4f5a8593f8b250e1f57ec
Matt <matthew92902@gmail.com>
2023-04-17 21:59:12 -0400

    Added Character Controller and Test Room

---
+++ Speed Game/Assets/Runtime/_Scripts/Core/Player/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpeedGame.Core.Player
+{
+    public class Player : MonoBehaviour
+    {
+        // Definition:
+        // This class will handle the code for the player:
+
+        #region Variables:
+
+        public Transform hitBoxTransform;
+
+        #endregion
+
+        #region Methods:
+
+        #endregion
+
+
+    }
+}
